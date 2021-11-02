3ad36f7d84e43e47b8436137413cb3b41d70a85c
jsungmin6 <jsungmin506@gmail.com>
2021-11-03 01:01:31 +0900

    effective java init, item33

---
+++ Effective/src/main/java/item33/Favorites1.java
@@ -0,0 +1,38 @@
+package item33;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+public class Favorites1 {
+    // 코드 33-3 타입 안전 이종 컨테이너 패턴 - 구현 (200쪽)
+    private Map<Class<?>, Object> favorites = new HashMap<>();
+
+    public <T> void putFavorite(Class<T> type, T instance) {
+        favorites.put(Objects.requireNonNull(type), instance);
+    }
+
+    public <T> T getFavorite(Class<T> type) {
+        return type.cast(favorites.get(type));
+    }
+
+    // 코드 33-2 타입 안전 이종 컨테이너 패턴 - 클라이언트 (199쪽)
+    public static void main(String[] args) {
+        Favorites1 f = new Favorites1();
+
+        f.putFavorite(String.class, "Java");
+        f.putFavorite(Integer.class, 0xcafebabe);
+        f.putFavorite(Class.class, Favorites1.class);
+
+        String favoriteString = f.getFavorite(String.class);
+        int favoriteInteger = f.getFavorite(Integer.class);
+
+        System.out.println(favoriteString);
+        System.out.println(favoriteInteger);
+
+        Class<?> favoriteClass = f.getFavorite(Class.class);
+
+        System.out.printf("%s %x %s%n", favoriteString,
+                favoriteInteger, favoriteClass.getName());
+    }
+}
+
+
