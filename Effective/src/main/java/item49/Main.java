e540a6f4e961bc64ac356afcf03d534e758ec95f
chess.min <chess.min@kakaocorp.com>
2021-12-01 09:46:50 +0900

    effective item item 49

--- Effective/src/main/java/item49/Main.java
+++ Effective/src/main/java/item49/Main.java
@@ -1,9 +1,14 @@
 package item49;
 
+import java.math.BigInteger;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
 
         assert false;
+
+//        Objects.requireNonNull(nullTest,"널이다.");
 
         System.out.println("hi");
 
